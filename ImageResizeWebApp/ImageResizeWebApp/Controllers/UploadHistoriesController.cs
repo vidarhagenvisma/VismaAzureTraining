8cda3910a7e93be780c5163e5b2d9cd013bf4042
Vidar Hagen <vidarh@hotmail.com>
2018-02-27 10:18:55 +0100

    Updated sort order

--- ImageResizeWebApp/ImageResizeWebApp/Controllers/UploadHistoriesController.cs
+++ ImageResizeWebApp/ImageResizeWebApp/Controllers/UploadHistoriesController.cs
@@ -22,7 +22,7 @@ namespace ImageResizeWebApp.Controllers
         // GET: UploadHistories
         public async Task<IActionResult> Index()
         {
-            return View(await _context.UploadHistory.ToListAsync());
+            return View(await _context.UploadHistory.OrderByDescending(m =>m.date).ToListAsync());
         }
 
         // GET: UploadHistories/Details/5
@@ -43,107 +43,8 @@ namespace ImageResizeWebApp.Controllers
             return View(uploadHistory);
         }
 
-        // GET: UploadHistories/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: UploadHistories/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,URL,IP,date")] UploadHistory uploadHistory)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(uploadHistory);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(uploadHistory);
-        }
-
-        // GET: UploadHistories/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var uploadHistory = await _context.UploadHistory.SingleOrDefaultAsync(m => m.ID == id);
-            if (uploadHistory == null)
-            {
-                return NotFound();
-            }
-            return View(uploadHistory);
-        }
-
-        // POST: UploadHistories/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,URL,IP,date")] UploadHistory uploadHistory)
-        {
-            if (id != uploadHistory.ID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(uploadHistory);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UploadHistoryExists(uploadHistory.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(uploadHistory);
-        }
-
-        // GET: UploadHistories/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var uploadHistory = await _context.UploadHistory
-                .SingleOrDefaultAsync(m => m.ID == id);
-            if (uploadHistory == null)
-            {
-                return NotFound();
-            }
-
-            return View(uploadHistory);
-        }
-
-        // POST: UploadHistories/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var uploadHistory = await _context.UploadHistory.SingleOrDefaultAsync(m => m.ID == id);
-            _context.UploadHistory.Remove(uploadHistory);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+  
 
         private bool UploadHistoryExists(int id)
         {
